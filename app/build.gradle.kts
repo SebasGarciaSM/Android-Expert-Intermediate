1fb5043baed4cdf204e5f84a0c12599ce4ac7560
SebasGarciaSM <75601981+SebasGarciaSM@users.noreply.github.com>
2025-03-05 17:50:14 -0600

    retrofit implementation build types

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,12 +21,22 @@ android {
     }
 
     buildTypes {
-        release {
+        getByName("release") {
             isMinifyEnabled = false
+            isDebuggable = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+
+            resValue("string", "appTitle", "HoroscApp")
+
+            buildConfigField("String", "BASE_URL", "\"https://newastro.vercel.app/\"")
+        }
+        getByName("debug") {
+            isDebuggable = true
+            resValue("string", "appTitle", "[Debug] HoroscApp")
+            buildConfigField("String", "BASE_URL", "\"https://newastro-debug.vercel.app/\"")
         }
     }
     compileOptions {
@@ -38,6 +48,7 @@ android {
     }
     buildFeatures {
         viewBinding = true
+        buildConfig = true
     }
 }
 
@@ -51,8 +62,13 @@ dependencies {
     implementation(libs.hilt.android)
     kapt(libs.hilt.compiler)
 
-    //Safeargs
+    //Retrofit
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.gson)
 
+    //Interceptor
+    implementation(libs.okhttp)
+    implementation(libs.okhttp.logging.interceptor)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
--- app/src/main/java/com/example/horoscapp/ui/detail/HoroscopeDetailActivity.kt
+++ app/src/main/java/com/example/horoscapp/ui/detail/HoroscopeDetailActivity.kt
@@ -6,12 +6,17 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.navigation.NavArgs
+import androidx.core.view.isVisible
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.navArgs
 import com.example.horoscapp.R
 import com.example.horoscapp.databinding.ActivityHoroscopeDetailBinding
-import com.example.horoscapp.databinding.ActivityMainBinding
+import com.example.horoscapp.domain.model.HoroscopeModel
+import com.example.horoscapp.domain.model.HoroscopeModel.*
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class HoroscopeDetailActivity : AppCompatActivity() {
@@ -31,5 +36,61 @@ class HoroscopeDetailActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        horoscopeDetailViewModel.getHoroscope(args.type)
+        initUI()
+    }
+
+    private fun initUI() {
+        initListeners()
+        initUIState()
+    }
+
+    private fun initListeners() {
+        binding.ivBack.setOnClickListener { onBackPressedDispatcher.onBackPressed() }
+    }
+
+    private fun initUIState() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                horoscopeDetailViewModel.state.collect {
+                    when (it) {
+                        HoroscopeDetailState.Loading -> loadingState()
+                        is HoroscopeDetailState.Error -> errorState()
+                        is HoroscopeDetailState.Success -> successState(it)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun loadingState() {
+        binding.progressBar.isVisible = true
+    }
+
+    private fun errorState() {
+        binding.progressBar.isVisible = false
+    }
+
+    private fun successState(state: HoroscopeDetailState.Success) {
+        binding.progressBar.isVisible = false
+        binding.tvTitle.text = state.sign
+        binding.tvBody.text = state.prediction
+
+        val image = when (state.horoscopeModel) {
+            Aries -> R.drawable.detail_aries
+            Taurus -> R.drawable.detail_taurus
+            Gemini -> R.drawable.detail_gemini
+            Cancer -> R.drawable.detail_cancer
+            Leo -> R.drawable.detail_leo
+            Virgo -> R.drawable.detail_virgo
+            Libra -> R.drawable.detail_libra
+            Scorpio -> R.drawable.detail_scorpio
+            Sagittarius -> R.drawable.detail_sagittarius
+            Capricorn -> R.drawable.detail_capricorn
+            Aquarius -> R.drawable.detail_aquarius
+            Pisces -> R.drawable.detail_pisces
+        }
+
+        binding.ivDetail.setImageResource(image)
     }
 }
